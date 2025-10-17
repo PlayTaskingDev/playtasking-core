4f524e70b70b103877f69406f25477c1f139f799
Alberto P. <desarrollo@playtasking.com>
2025-10-17 10:03:25 -0600

    Update splash.blade.php

--- resources/views/dashboard/campaigns/splash.blade.php
+++ resources/views/dashboard/campaigns/splash.blade.php
@@ -49,7 +49,7 @@
                                 </div>
                             </div>
                         @endforeach
-                    
+                    @endif
                 </div>
             </div>
         </div>
