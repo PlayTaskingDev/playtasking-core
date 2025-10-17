093c1c1bc4a34e1377f39ec8d49d3a24fdd0840b
Alberto P. <desarrollo@playtasking.com>
2025-10-17 14:58:59 -0600

    UPDATE: Ticket amount from 260 to 500 and spearation in Mmory quiz

--- app/Http/Requests/StoreTicketRequest.php
+++ app/Http/Requests/StoreTicketRequest.php
@@ -37,7 +37,7 @@ class StoreTicketRequest extends FormRequest
             'transaction_number'    => ['required','regex:/^[A-Z0-9]+$/'],
             'transaction_date'      => ['required','date_format:Y-m-d','after_or_equal:' . $campaign->init_date, 'before_or_equal:' . $today->toDateString()],
             'store'                 => ['required','regex:/^[A-Z0-9]+$/'],
-            'amount'                => ['required','numeric','min:260'],
+            'amount'                => ['required','numeric','min:500'],
             //'ticket'                => [Rule::filepond(['required','image','max:12000'])],
             'ticket'                => ['required','file','max:12000',],
             'ticket_answer'         => [Rule::requiredIf(fn () => get_app_setting('tickets_quiz_validation')),'exists:ticket_answers,id'],
