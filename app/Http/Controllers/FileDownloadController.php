181cf2e92164c9575aa28b352523dc41a7141be8
Alberto P. <desarrollo@playtasking.com>
2025-11-21 15:27:48 -0600

    Update FileDownloadController.php

--- app/Http/Controllers/FileDownloadController.php
+++ app/Http/Controllers/FileDownloadController.php
@@ -7,8 +7,6 @@ use Illuminate\Http\Request;
 class FileDownloadController extends Controller
 {
     public function index(){
-         return response()->download(
-            storage_path('app/private/entraalmasalla-users.zip')
-        );
+        return '<h2>Descargar</h2> <a href='.response()->download(storage_path('app/private/entraalmasalla-users.zip')).'>aquí</a>';
     }
 }
