660efc985bdc47f9f69562faaedf1fb0eadf83f0
Alberto P. <desarrollo@playtasking.com>
2025-11-21 16:08:50 -0600

    Update filedownload.blade.php

--- resources/views/filedownload.blade.php
+++ resources/views/filedownload.blade.php
@@ -8,15 +8,13 @@
                     @if($errors->any())
                         <div style="color:red;">{{ $errors->first() }}</div>
                     @endif
-
-                    <form method="POST" action="{{ route('file.download',['tenant' => tenant('id')]) }}">
-                        @csrf
-                        <label>Contraseña:</label>
-                        <input type="password" name="password" required>
-
-                        <button type="submit">Descargar</button>
-                    </form>
-               
+               <form class="max-w-sm mx-auto" method="POST" action="{{ route('file.download',['tenant' => tenant('id')]) }}">
+                    <div class="mb-5">
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña:</label>
+                        <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                    </div>
+                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Descargar</button>
+                </form>
             </div>
         </div>
     </div>
