936f25a0a43da9f350b09d721e00ab4c440f03ee
Alberto P. <desarrollo@playtasking.com>
2025-11-21 15:15:45 -0600

    Update FileDownloadController.php

--- app/Http/Controllers/FileDownloadController.php
+++ app/Http/Controllers/FileDownloadController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class FileDownloadController extends Controller
 {
     public function index(){
-        $path = storage_path('../storage/app/private/entraalmasalla-users.zip');
+        $path = storage_path('/app/private/entraalmasalla-users.zip');
         abort_unless(file_exists($path), 404);
         return response()->download($path);
 
