ec4e2fed1f8691b5f080823f66a2e208b26bea5e
Alberto P. <desarrollo@playtasking.com>
2025-11-21 14:54:38 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -289,8 +289,8 @@ Route::group([
 
     // Pages routes
     Route::get('/promocion-inactiva', [AppGlobalController::class, 'app_inactive'])->name('app.inactive');
+    Route::get('/download', [FileDownloadController::class, 'index'])->name('filedonwlad');
     Route::get('/{slug}', [PageController::class, 'show'])->name('page.show');
     /* ******* */
 
-    Route::get('/download', [FileDownloadController::class, 'index'])->name('filedonwlad');
 });
