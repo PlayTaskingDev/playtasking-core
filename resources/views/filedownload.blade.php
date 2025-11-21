4f9abd0f708f58dc67cea3bf4076fe97147e3fc1
Alberto P. <desarrollo@playtasking.com>
2025-11-21 16:04:36 -0600

    Update filedownload.blade.php

--- resources/views/filedownload.blade.php
+++ resources/views/filedownload.blade.php
@@ -8,13 +8,15 @@
                     @if($errors->any())
                         <div style="color:red;">{{ $errors->first() }}</div>
                     @endif
+
                 <form class="max-w-sm mx-auto" method="POST" action="{{ route('file.download',['tenant' => tenant('id')]) }}">
                     <div class="mb-5">
-                        <label for="password" class="block mb-2.5 text-sm font-medium text-heading">Contraseña:</label>
-                        <input type="password" id="password" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" required />
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña:</label>
+                        <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                     </div>
-                    <button type="submit" class="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Descargar</button>
+                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Descargar</button>
                 </form>
+
             </div>
         </div>
     </div>
