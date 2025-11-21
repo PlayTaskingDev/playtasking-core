2b955fbadae7480438021830ef50b6dec15584d6
Alberto P. <desarrollo@playtasking.com>
2025-11-21 15:58:40 -0600

    ADD:Route to download file

--- app/Http/Controllers/FileDownloadController.php
+++ app/Http/Controllers/FileDownloadController.php
@@ -12,4 +12,22 @@ class FileDownloadController extends Controller
         //     storage_path('app/private/entraalmasalla-users.zip')
         // );
     }
+
+       public function download(Request $request)
+    {
+        // CONTRASEÑA FIJA
+        $password = "Promo2024";
+
+        if ($request->password !== $password) {
+            return back()->withErrors(['password' => 'La contraseña es incorrecta']);
+        }
+
+        $file = storage_path('app/private/entraalmasalla-users.zip');
+
+        if (!file_exists($file)) {
+            abort(404);
+        }
+
+        return response()->download($file);
+    }
 }
--- resources/views/filedownload.blade.php
+++ resources/views/filedownload.blade.php
@@ -3,7 +3,22 @@
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
         <div class="w-full dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8 bg-white rounded-lg">
-               <h2>Descargar <a href="{{ route('filedownload',['tenant' => tenant('id')]) }}" class="underline">aquí</a></h2>
+                    <h3>Descargar archivo</h3>
+
+                    @if($errors->any())
+                        <div style="color:red;">{{ $errors->first() }}</div>
+                    @endif
+
+                    <form method="POST" action="{{ route('file.download',['tenant' => tenant('id')]) }}">
+                        @csrf
+                        <label>Contraseña:</label>
+                        <input type="password" name="password" required>
+
+                        <button type="submit">Descargar</button>
+                    </form>
+               
+               
+                    <h2>Descargar <a href="{{ route('filedownload',['tenant' => tenant('id')]) }}" class="underline">aquí</a></h2>
             </div>
         </div>
     </div>
