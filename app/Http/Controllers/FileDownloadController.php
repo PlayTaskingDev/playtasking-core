2f96c9dc4fc60a93383e147b1e6ef894a4c1b67c
Alberto P. <desarrollo@playtasking.com>
2025-11-21 15:12:06 -0600

    Update FileDownloadController.php

--- app/Http/Controllers/FileDownloadController.php
+++ app/Http/Controllers/FileDownloadController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 class FileDownloadController extends Controller
 {
     public function index(){
-         return view('filedownload',[
-            'title'         => 'File Download',
-        ]);
+         return response()->download(
+            global_storage_path('private/entraalmasalla-users.zip')
+        );
     }
 }
