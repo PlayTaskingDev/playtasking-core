1465377848803c8241242e3faa14b79e3b660ec7
Alberto P. <desarrollo@playtasking.com>
2025-11-21 15:49:21 -0600

    Update filedownload.blade.php

--- resources/views/filedownload.blade.php
+++ resources/views/filedownload.blade.php
@@ -1,3 +1,10 @@
+
 <x-guest-layout>
-    <h2>Descargar <a href="{{ route('filedownload',['tenant' => tenant('id')]) }}">aquí</a></h2>
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
+        <div class="w-full dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8 bg-white rounded-lg">
+               <h2>Descargar <a href="{{ route('filedownload',['tenant' => tenant('id')]) }}">aquí</a></h2>
+            </div>
+        </div>
+    </div>
 </x-guest-layout>
