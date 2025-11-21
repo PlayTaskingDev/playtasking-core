83d50a264a7d7e30fec94087ec9fea7c24520640
Alberto P. <desarrollo@playtasking.com>
2025-11-21 16:13:54 -0600

    Update FileDownloadController.php

--- app/Http/Controllers/FileDownloadController.php
+++ app/Http/Controllers/FileDownloadController.php
@@ -16,7 +16,7 @@ class FileDownloadController extends Controller
        public function download(Request $request)
     {
         // CONTRASEÑA FIJA
-        $password = "3ntr4alm4sall42025@";
+        $password = "3ntr4alm4sall42025@12";
 
         if ($request->password !== $password) {
             return back()->withErrors(['password' => 'La contraseña es incorrecta']);
