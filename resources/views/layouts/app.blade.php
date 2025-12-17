821dd14eb95d03eddbb694822101036f97a406d4
Alberto P. <desarrollo@playtasking.com>
2025-12-17 14:45:51 -0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,12 +62,6 @@
             color: {{get_app_setting('cards_font_color')}};
         }
 
-         .icon-tab-ranking {
-            color: {{get_app_setting('cards_font_color')}};
-            margin-right: 4px;
-        }
-
-
         .game-card p {
             color: {{get_app_setting('cards_font_color')}}
         }
