b57f979ffb1aa57e38cfbe21bfc36f8bceea57ed
Alberto P. <desarrollo@playtasking.com>
2026-03-13 19:08:49 -0600

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,8 +31,6 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        dd(get_app_setting('allow_city'));
-        dd($request);
         $request->validate([
             'name'                  => ['required', 'string', 'max:255'],
             'email'                 => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
