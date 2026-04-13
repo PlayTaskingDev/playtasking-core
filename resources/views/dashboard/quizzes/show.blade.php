7275b4527521e19cd872d14fd16a1f4ebb540998
PlayTaskingDev <desarrollo@playtasking.com>
2026-04-12 19:10:47 -0600

    Update show.blade.php

--- resources/views/dashboard/quizzes/show.blade.php
+++ resources/views/dashboard/quizzes/show.blade.php
@@ -53,7 +53,7 @@
                             {{$question->title}}
                         </p>
                             <div id="question_{{ $loop->iteration }}"
-                                class="section-question grid gap-4 sm:grid-cols-1 {{ $loop->first ? '' : 'opacity-0 hidden' }}">
+                                class="section-question grid gap-4 sm:grid-cols-1 {{ $loop->first ? '' : 'opacity-0' }}">
                                 <div>
                                     <ul
                                         class="w-auto text-sm font-medium text-white dark:bg-gray-700 dark:border-gray-600 dark:text-white quiz-answers">
