68fa4951b61353b0632f25909524b955f2c90e4a
PlayTaskingDev <desarrollo@playtasking.com>
2026-04-27 08:25:30 -0600

    Refactor target zones and container layout

--- resources/views/dashboard/penal_games/show.blade.php
+++ resources/views/dashboard/penal_games/show.blade.php
@@ -29,7 +29,6 @@
       position: absolute;
       width: 100%;
       height: 100%;
-      object-fit: contain;
       z-index: 0;
     }
 
@@ -127,13 +126,22 @@
       top: 58%;
       left: 50.40%;
       transform: translate(-50%, -50%);
-      width: 320px;
+      width: 58%;
       height: 172px;
       z-index: 4;
       cursor: crosshair;
       background-color: rgba(255, 255, 0, 0.0); 
     }
-
+    .target-container{
+        position: absolute;
+        top: 58%;
+        left: 50.40%;
+        transform: translate(-50%, -50%);
+        width: 58%;
+        height: 172px;
+        z-index: 5;
+    }
+        
     .target-zone {
       position: absolute;
       width: 60px; 
@@ -197,19 +205,20 @@
                               <img src="https://i.imgur.com/aX1zBPZ.png" alt="Goalkeeper" class="goalkeeper" />
                               <img src="https://i.imgur.com/qarbcqB.png" alt="Ball" class="ball" />
                               <div class="goal-area"></div>
-
-                              <!-- line 2 -->
-                              <div class="target-zone clickable" data-zone="haut-gauche" style="top: 360px; left: 95px;"></div>
-                              <div class="target-zone clickable" data-zone="haut-centre" style="top: 360px; left: 220px;"></div>
-                              <div class="target-zone clickable" data-zone="haut-droite" style="top: 360px; left: 330px;"></div>
-                              <!-- line 1 -->
-                              <div class="target-zone clickable" data-zone="milieu-gauche" style="top: 425px; left: 95px;"></div>
-                              <div class="target-zone clickable" data-zone="milieu-centre" style="top: 425px; left: 215px;"></div>
-                              <div class="target-zone clickable" data-zone="milieu-droite" style="top: 425px; left: 330px;"></div>
-                              <!-- Line 2 -->
-                              <div class="target-zone clickable" data-zone="bas-gauche" style="top: 490px; left: 95px;"></div>
-                              <!--<div class="target-zone clickable" data-zone="bas-centre" style="top: 490px; left: 215px;"></div>-->
-                              <div class="target-zone clickable" data-zone="bas-droite" style="top: 490px; left: 330px;"></div>
+                              <div class="target-container">  
+                                  <!-- line 1 -->
+                                  <div class="target-zone clickable" data-zone="haut-gauche" style="top: 360px; left: 95px;"></div>
+                                  <div class="target-zone clickable" data-zone="haut-centre" style="top: 360px; left: 220px;"></div>
+                                  <div class="target-zone clickable" data-zone="haut-droite" style="top: 360px; left: 330px;"></div>
+                                  <!-- line 2 -->
+                                  <div class="target-zone clickable" data-zone="milieu-gauche" style="top: 425px; left: 95px;"></div>
+                                  <div class="target-zone clickable" data-zone="milieu-centre" style="top: 425px; left: 215px;"></div>
+                                  <div class="target-zone clickable" data-zone="milieu-droite" style="top: 425px; left: 330px;"></div>
+                                  <!-- Line 3 -->
+                                  <div class="target-zone clickable" data-zone="bas-gauche" style="top: 490px; left: 95px;"></div>
+                                  <!--<div class="target-zone clickable" data-zone="bas-centre" style="top: 490px; left: 215px;"></div>-->
+                                  <div class="target-zone clickable" data-zone="bas-droite" style="top: 490px; left: 330px;"></div>
+                              </div>
                             </div>
                             <div id="message" class="text-white text-center text-2xl text-shadow font-bold py-12"></div>
                         </div>
