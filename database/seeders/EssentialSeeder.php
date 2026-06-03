2141ed5e61fb420170dbd9b3fbaac1d370bb0eb8
PlayTaskingDev <desarrollo@playtasking.com>
2026-06-03 14:26:04 -0600

    UPDATE: Migrations and seeders consolidated

--- database/seeders/EssentialSeeder.php
+++ database/seeders/EssentialSeeder.php
@@ -2,18 +2,88 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Award;
+use App\Models\AwardCode;
+use App\Models\Campaign;
+use App\Models\CampaignSplashPage;
+use App\Models\ContentType;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
-class OptionsTableSeeder extends Seeder
+class EssentialSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        DB::table('options')->insert(
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        DB::table('award_codes')->truncate();
+        DB::table('award_user')->truncate();
+        CampaignSplashPage::truncate();
+        Award::truncate();
+        AwardCode::truncate();
+        Campaign::truncate();
+
+        DB::table('content_types')->insert(
+            [
+                [
+                    'id'            => Str::uuid(),
+                    'name'          => 'Games',
+                    'system_name'   => 'games',
+                    'description'   => 'In this section you can correctly answer the trivia, solve the memory puzzle and share it on your networks to win instant prizes.',
+                    'icon'          => '/storage/dummy_assets/gaming-white.png',
+                    'icon_active'   => '/storage/dummy_assets/gaming-active.png',
+                    'section_banner'=> '/storage/dummy_assets/600x200.png',
+                    'gradient_1'    => '#000000',
+                    'gradient_2'    => '#000000',
+                ],
+                [
+                    'id'            => Str::uuid(),
+                    'name'          => 'Register tickets',
+                    'system_name'   => 'tickets',
+                    'description'   => 'In this section for entering your purchase receipt, enter the largest amount of your purchases and the more you enter, the more chances you will have to win.',
+                    'icon'          => '/storage/dummy_assets/tickets-white.png',
+                    'icon_active'   => '/storage/dummy_assets/tickets-active.png',
+                    'section_banner'=> '/storage/dummy_assets/600x200.png',
+                    'gradient_1'    => '#000000',
+                    'gradient_2'    => '#000000',
+                ],
+                [
+                    'id'            => Str::uuid(),
+                    'name'          => 'Register coupons',
+                    'system_name'   => 'coupons',
+                    'description'   => 'In this section for entering codes, enter the largest number of available coupons and the more you enter, the more chances you will have to win.',
+                    'icon'          => '/storage/dummy_assets/coupon-dark.png',
+                    'icon_active'   => '/storage/dummy_assets/coupon-active.png',
+                    'section_banner'=> '/storage/dummy_assets/600x200.png',
+                    'gradient_1'    => '#000000',
+                    'gradient_2'    => '#000000',
+                ],
+            ]
+        );
+
+        $campaign = Campaign::factory()->create();
+        $content_type_games = ContentType::where('system_name','games')->first();
+        $content_type_tickets = ContentType::where('system_name','tickets')->first();
+        $content_type_coupons = ContentType::where('system_name','coupons')->first();
+
+        CampaignSplashPage::create([
+            'campaign_id'           => $campaign->id,
+            'featured_video_url'    => 'https://www.youtube.com/embed/oNyDux6c1dQ?si=JLR30wAuSO3NrQ3x',
+            'featured_image_url'    => null,
+            'instructions'          => '<p>Below you can participate for incredible <strong>instant wins</strong> and also participate for an <strong>AUDI A1 2024</strong>.</p>',
+        ]);
+
+        $campaign->content_types()->attach([$content_type_games->id,$content_type_tickets->id,$content_type_coupons->id]);
+
+       
+         DB::table('options')->insert(
             [
                 [
                     'option_name'    => 'app_name',
@@ -313,7 +383,10 @@ class OptionsTableSeeder extends Seeder
                 
             ]
         );
+      
+
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
     }
 }
-
-
