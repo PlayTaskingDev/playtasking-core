952ff7d3eb2601cffea2e90999fdcec1133e44d1
PlayTaskingDev <desarrollo@playtasking.com>
2026-06-24 21:16:21 -0600

    Update PuzzleController.php

--- app/Http/Controllers/PuzzleController.php
+++ app/Http/Controllers/PuzzleController.php
@@ -130,7 +130,7 @@ class PuzzleController extends Controller
         } else {
             return redirect()->route('game.out_of_coupons', ['tenant' => tenant('id')]);
         }
-        
+        dd($query);
         if ($query) {
             return response()->json(['status' => 'success'], 200);
         } else {
