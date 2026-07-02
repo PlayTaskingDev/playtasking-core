e6057aaddaf50a35cfdb729063839f2ecf61cde8
PlayTaskingDev <desarrollo@playtasking.com>
2026-07-02 17:47:32 -0600

    Update StoreTicketRequest.php

--- app/Http/Requests/StoreTicketRequest.php
+++ app/Http/Requests/StoreTicketRequest.php
@@ -37,7 +37,7 @@ class StoreTicketRequest extends FormRequest
             'transaction_number'    => ['required','regex:/^[A-Z0-9]+$/'],
             'transaction_date'      => ['required','date_format:Y-m-d','after_or_equal:' . $campaign->init_date, 'before_or_equal:' . $today->toDateString()],
             'store'                 => ['required','regex:/^[A-Z0-9]+$/'],
-            'amount'                => ['required','numeric','min:230'],
+            'amount'                => ['required','numeric','min:120'],
             //'ticket'                => [Rule::filepond(['required','image','max:12000'])],
             'ticket'                => ['required','file','max:12000',],
             'ticket_answer'         => [Rule::requiredIf(fn () => get_app_setting('tickets_quiz_validation')),'exists:ticket_answers,id'],
@@ -47,7 +47,7 @@ class StoreTicketRequest extends FormRequest
      public function messages(): array
     {
         return [
-            'amount' => 'Compra mínima de $230 pesos.',
+            'amount' => 'Compra mínima de $120 pesos.',
         ];
     }
 }
